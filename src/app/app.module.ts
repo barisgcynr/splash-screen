49a80b7c0c270f4b9c5c31691af8b60c11057edd
Barış <barisgcynr@gmail.com>
2021-07-09 16:19:28 +0300

    v0.0.1

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SplashScreenComponent } from './components/splash-screen/splash-screen.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SplashScreenComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
